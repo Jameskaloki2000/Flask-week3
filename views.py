dc8508d3d0350c385ea9ab6782d50f8da6e2fe76
James Kaloki <kaloki2012@gmail.com>
2022-05-11 03:08:11 +0300

    created a class for the user

--- views.py
+++ views.py
@@ -1,8 +1,21 @@
 from flask import render_template,Flask,url_for,flash,redirect
-
+from flask_sqlalchemy import  SQLAlchemy
+import sqlalchemy
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Jackboy'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db=sqlalchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 posts = [
     {
         'author': 'James',
