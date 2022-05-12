2cfaca38c8d812351c9f72e28476ab358ea9e7b3
James Kaloki <kaloki2012@gmail.com>
2022-05-12 07:52:04 +0300

    deleted view.py and rearranged folder structre

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
@@ -11,6 +12,7 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
